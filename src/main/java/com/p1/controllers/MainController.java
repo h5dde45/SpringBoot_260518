55a2f8d9734f4bfa9de0f4e9c5343b1e09d82c9f
tmvf <tmvf@yandex.ru>
2018-05-28 15:27:59 +0300

    izm_4

--- src/main/java/com/p1/controllers/MainController.java
+++ src/main/java/com/p1/controllers/MainController.java
@@ -4,17 +4,26 @@ import com.p1.domain.Message;
 import com.p1.domain.User;
 import com.p1.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting() {
@@ -42,11 +51,23 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Model model) {
+            Model model,
+            @RequestParam("file") MultipartFile file) throws IOException {
         Message message = new Message(text, tag, user);
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
         messageRepo.save(message);
         model.addAttribute("messages", messageRepo.findAll());
-        model.addAttribute("filter", "");
+//        model.addAttribute("filter", "");
 
         return "main";
     }
--- src/main/resources/templates/greeting.ftl
+++ src/main/resources/templates/greeting.ftl
@@ -1,6 +1,5 @@
 <#import "parts/common.ftl" as c>
 
 <@c.page>
-<div>hello, user</div>
-<a href="/main">Main Page</a>
+<div>Welcome page...</div>
 </@c.page>
